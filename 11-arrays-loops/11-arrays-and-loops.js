8024e08e749828df1f63f12fc15207b84fe26c9d
josheeen <therealjosh26@gmail.com>
2026-02-01 17:37:21 +0800

    Added folder to make it look cleaner.

---
+++ 11-arrays-loops/11-arrays-and-loops.js
@@ -0,0 +1,34 @@
+// const todoList = [
+//   'make dinner', 'wash dishes', 'watch youtube'
+// ]
+
+// for (let index = 0; index <= todoList.length - 1; index++) {
+//   console.log(todoList[index])
+// }
+// console.log(todoList.length)
+
+
+const nums = [
+  1, 1, 3
+]
+
+let total = 0;
+
+// for (let i = 0; i < nums.length; i++) {
+//   console.log(i)
+
+//   console.log(nums[i])
+
+//   total = total + nums[i]
+//   console.log(total)
+//   console.log('')
+// }
+// console.log(total)
+
+const numsDoubled = [
+
+]
+for (let i = 0; i < nums.length; i++) {
+  numsDoubled.push(nums[i] * 2);
+}
+console.log(numsDoubled)
